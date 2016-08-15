1a445fc987a778260e6b0ac9fefbcf69086f0544
Hemanth Yamijala <hyamijala@hortonworks.com>
2016-08-15 17:34:52 +0530

    Added binary distribution target so tarball can be shipped for testing to different environments

--- native-jetty-websocket-example/bin/run-publishers.sh
+++ native-jetty-websocket-example/bin/run-publishers.sh
@@ -2,8 +2,22 @@
 START_PORT=$1
 NUM_INSTANCES=$2
 END_PORT=$((START_PORT + NUM_INSTANCES - 1))
+BIN_DIR=`dirname $0`
+LOG_DIR=${BIN_DIR}/../logs
+LIB_DIR=${BIN_DIR}/../lib
+
+if  [ $# -ne 2 ]
+then
+    echo $0 start_port num_instances;
+    exit -1;
+fi
+
+if [ ! -d ${LOG_DIR} ]
+then
+    mkdir -p ${LOG_DIR};
+fi
 
 for i in `seq ${START_PORT} ${END_PORT}`
 do
-    mvn exec:java -Dexec.mainClass=org.eclipse.jetty.demo.pubsub.PublisherServer -Dexec.args="localhost:${i}" &> logs/localhost_${i}.log &
+    java -cp ${BIN_DIR}/../lib/native-jetty-websocket-example-1.0-SNAPSHOT-jar-with-dependencies.jar org.eclipse.jetty.demo.pubsub.PublisherServer localhost:${i} &> ${BIN_DIR}/../logs/localhost_${i}.log &
 done
