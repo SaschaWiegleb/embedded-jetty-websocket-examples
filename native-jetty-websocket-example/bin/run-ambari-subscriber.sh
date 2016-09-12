bdf8f1d874c052d5c94e12c711b5922af4600b02
Hemanth Yamijala <hyamijala@hortonworks.com>
2016-09-12 19:29:25 +0530

    Added support for secure websockets in communicating to Ambari

--- native-jetty-websocket-example/bin/run-ambari-subscriber.sh
+++ native-jetty-websocket-example/bin/run-ambari-subscriber.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-if  [ $# -ne 1 ]
+if  [ $# -ne 2 ]
 then
-    echo $0 server_ip:port;
+    echo $0 server_ip:port plain_or_secure;
     exit -1;
 fi
 
 SERVER_IP_PORT=$1
+PLAIN_OR_SECURE=$2
+
 BIN_DIR=`dirname $0`
 LOG_DIR=${BIN_DIR}/../logs
 LIB_DIR=${BIN_DIR}/../lib
@@ -16,4 +18,4 @@ then
     mkdir -p ${LOG_DIR};
 fi
 
-java -cp ${LIB_DIR}/native-jetty-websocket-example-1.0-SNAPSHOT-jar-with-dependencies.jar org.eclipse.jetty.demo.pubsub.AmbariSubscriberClient ${SERVER_IP_PORT} &> ${LOG_DIR}/subscriber.log &
+java -cp ${LIB_DIR}/native-jetty-websocket-example-1.0-SNAPSHOT-jar-with-dependencies.jar org.eclipse.jetty.demo.pubsub.AmbariSubscriberClient ${SERVER_IP_PORT} ${PLAIN_OR_SECURE} &> ${LOG_DIR}/ambari_subscriber.log &
--- native-jetty-websocket-example/src/main/java/org/eclipse/jetty/demo/pubsub/AmbariSubscriberClient.java
+++ native-jetty-websocket-example/src/main/java/org/eclipse/jetty/demo/pubsub/AmbariSubscriberClient.java
@@ -7,11 +7,16 @@ import java.net.URI;
 
 public class AmbariSubscriberClient {
     public static void main(String[] args) throws Exception {
-        String defaultPublisher = "c6401.ambari.apache.org:8080";
-        if (args.length >= 1) {
-            defaultPublisher = args[0];
+        String serverAndPort = null;
+        boolean isPlain = false;
+        if (args.length != 2) {
+            System.out.println("Usage: AmbariSubscriberClient <ip:port> <plain|secure>");
+            System.exit(-1);
+        } else {
+            serverAndPort = args[0];
+            isPlain = "plain".equals(args[1]);
         }
-        connectTo(getUrl(defaultPublisher));
+        connectTo(getUrl(serverAndPort, isPlain));
     }
 
     private static void connectTo(String url) throws Exception {
@@ -22,8 +27,9 @@ public class AmbariSubscriberClient {
         webSocketClient.open(uri, new SubscriberSocket(url));
     }
 
-    private static String getUrl(String address) {
-        return "ws://" + address + "/alerts";
+    private static String getUrl(String address, boolean isPlain) {
+        String scheme = isPlain ? "ws" : "wss";
+        return scheme + "://" + address + "/alerts";
     }
 
 }
